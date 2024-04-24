b532307457eef6bab3a48d977afe2c27c8fa2daa
Raul Campos <156030838+Queixas@users.noreply.github.com>
2024-04-24 12:00:50 -0300

    2.2 - Tipos de dado

---
+++ modulo 2/aula2_questao1.py
@@ -0,0 +1,10 @@
+#1) Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+#Armazene o valor 5 em uma variável e o valor 2 em outra variável
+#Imprima o tipo de dados dessas duas variáveis
+#Divida a primeira variável (com o valor 5) pela segunda variável (com o valor 2) e armazene o resultado em uma terceira variável
+#Imprima a terceira variável e também o seu tipo
+variavel1 = 5
+variavel2 = 2
+print(type(variavel1))
+divisao = variavel1 / variavel2
+print (divisao)
---
+++ modulo 2/aula2_questao2.py
@@ -0,0 +1,13 @@
+#2) Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+#Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+#Some os valores da segunda e terceira variável e armazene em outra variável.
+#Imprima todas as variáveis na ordem de criação e imprima também seus tipos."
+
+texto = "o resultado é"
+variavel1 = 10
+variavel2 = 3.5
+soma = variavel1 + variavel2
+print (texto, type(texto))
+print (variavel1, type(variavel1))
+print (variavel2, type(variavel2))
+print (soma, type(soma))
