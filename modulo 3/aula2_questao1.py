44b5b921a1ce0933adf7dd9c5fb30cfdd2fcb636
Raul Campos <156030838+Queixas@users.noreply.github.com>
2024-05-22 12:17:47 -0300

    Add files via upload

---
+++ modulo 3/aula2_questao1.py
@@ -0,0 +1,7 @@
+#1) Juliana e Cris querem entrar em um bar, mas só podem se ambos forem maiores de idade (>17).
+# Escreva um programa que solicite as idades de Juliana e Cris
+# e imprima True se ambas forem maiores de 17 anos, indicando que podem entrar no bar, e False caso contrário.
+
+juliana= int(input("Informe a idade de Juliana: "))
+cris= int(input("Informe a idade de Cris: "))
+print(juliana>=17 and cris>=17)
